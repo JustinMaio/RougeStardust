b6e1b2ec2150af9b87e831e6e57136855a7f14f9
Justin Maio <33429932+JustinMaio@users.noreply.github.com>
2025-01-10 16:31:14 -0700

    More skeleton setup
    Includes things like more ship and input setup and spline work

---
+++ RogueStardust/Source/RogueStardust/Private/RSPlayerShip.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "RSPlayerShip.h"
+
+#include "Components/StaticMeshComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+ARSPlayerShip::ARSPlayerShip()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	// Set this pawn to be controlled by the lowest-numbered player
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+
+    
+}
+
+// Called when the game starts or when spawned
+void ARSPlayerShip::BeginPlay()
+{
+	Super::BeginPlay();
+
+    //TEMP CAMERA attache code
+    // Create a dummy root component we can attach things to.
+    RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+    // Create a camera and a visible object
+    UCameraComponent* OurCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("OurCamera"));
+    // Attach our camera and visible object to our root component. Offset and rotate the camera.
+    OurCamera->SetupAttachment(RootComponent);
+    OurCamera->SetRelativeLocation(FVector(-250.0f, 0.0f, 250.0f));
+    OurCamera->SetRelativeRotation(FRotator(-45.0f, 0.0f, 0.0f));
+    GetMesh()->SetupAttachment(RootComponent);
+	
+}
+
+// Called every frame
+void ARSPlayerShip::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ARSPlayerShip::ShootPrimaryWeapon()
+{
+
+}
+
+void ARSPlayerShip::MoveYAxis(float axisValue)
+{
+
+}
+
+void ARSPlayerShip::MoveXAxis(float axisValue)
+{
+
+}
+
+
+// Called to bind functionality to input
+void ARSPlayerShip::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    // Respond when our "Grow" key is pressed or released.
+    InputComponent->BindAction("Fire", IE_Pressed, this, &ARSPlayerShip::ShootPrimaryWeapon);
+
+    // Respond every frame to the values of our two movement axes, "MoveX" and "MoveY".
+    InputComponent->BindAxis("MoveX", this, &ARSPlayerShip::MoveXAxis);
+    InputComponent->BindAxis("MoveY", this, &ARSPlayerShip::MoveYAxis);
+}
+
---
+++ RogueStardust/Source/RogueStardust/Public/RSPlayerShip.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "RSPlayerShip.generated.h"
+
+UCLASS()
+class ROGUESTARDUST_API ARSPlayerShip : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ARSPlayerShip();
+
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	void ShootPrimaryWeapon();
+	void MoveYAxis(float axisValue);
+	void MoveXAxis(float axisValue);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
